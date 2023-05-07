ac4e190a095e79a2bf9cb95b7aecc14179385f26
00_ademir <114341527+ademir06@users.noreply.github.com>
2023-05-07 16:21:47 +0600

    Loginpages (#2)
    * ooo
    
    * ssss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Header from "./components/header/Header";
+import LogIn from "./components/pages/LogIn";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Routes>
+                <Route path={'/'} element={<LogIn/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
