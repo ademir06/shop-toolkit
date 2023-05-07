c1276adbff6078af758b06ed34465bf75faebf69
Ademir__Dev <gaparov866@gmail.com>
2023-05-07 16:50:08 +0600

    yyyy

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface ILogin {
+    id: number
+    title: string
+    price: number
+    name: string
+    email: string
+    password: any
+    description: string
+    image: string
+    category: string
+}
