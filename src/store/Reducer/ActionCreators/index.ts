5345ce9bf9d11605620e1fb238e3dab0d8076f27
Adil <116545639+adilbek210505@users.noreply.github.com>
2023-05-07 22:24:22 +0600

    add finished telegram (#6)

--- src/store/Reducer/ActionCreators/index.ts
+++ src/store/Reducer/ActionCreators/index.ts
@@ -1,16 +1,29 @@
 import {appDispatch} from "../../index";
-import {getValue} from "../index";
+import {getLogin, getLoginError, getLoginSuccess, getMode, getValue} from "../index";
+import {CHAT_ID, TOKEN} from "../../../components/LiB";
+import axios from "axios";
 
 
 export const setValue = (e: any) => (dispatch: appDispatch) => {
     dispatch(getValue(e))
 }
 
-// export const formSliceCom = (newProduct: any) => async (dispatch: appDispatch) => {
-//     try {
-//         dispatch(getLogin())
-//         dispatch(getLoginSuccess(await axios.post(``, newProduct)))
-//     } catch (e) {
-//         dispatch(getLoginError("Error..."))
-//     }
-// }
+export const formSliceCom = (message: any) => async (dispatch: appDispatch) => {
+    try {
+        dispatch(getLogin())
+        dispatch(getLoginSuccess(
+            await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+                chat_id: CHAT_ID,
+                parse_mode: "html",
+                text: message
+            })
+        ))
+    } catch (e) {
+        dispatch(getLoginError("Error..."))
+    }
+}
+
+
+export const fetchMode = (mode: any) => (dispatch: appDispatch) => {
+    dispatch(getMode(mode))
+}
