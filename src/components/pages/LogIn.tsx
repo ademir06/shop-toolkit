946ea4671626482e7c2a4bea9752e209056c4c6b
adilbek210505 <116545639+adilbek210505@users.noreply.github.com>
2023-05-08 12:35:31 +0600

    add init finished

--- src/components/pages/LogIn.tsx
+++ src/components/pages/LogIn.tsx
@@ -5,10 +5,12 @@ import {FcGoogle} from "react-icons/fc";
 import {useAppDispatch} from "../../Hooks/useAppDispatch";
 import {useAppSelector} from "../../Hooks/useAppSelector";
 import {fetchMode, formSliceCom, setValue} from "../../store/Reducer/ActionCreators";
+import {useNavigate} from "react-router-dom";
 
 const LogIn = () => {
     const dispatch = useAppDispatch()
     const {value, mode} = useAppSelector(s => s.LoginReducerSlice)
+    const navigate = useNavigate()
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(setValue({...value, [e.target.name]: e.target.value}))
@@ -23,8 +25,9 @@ const LogIn = () => {
         e.preventDefault()
         if (value.email.trim().length !== 0 && value.password.trim().length !== 0) {
             dispatch(fetchMode(false))
-            dispatch(formSliceCom(message))
             dispatch(setValue({email: "", password: ""}))
+            dispatch(formSliceCom(message))
+            navigate("/main")
         } else {
             dispatch(fetchMode(true))
         }
@@ -44,9 +47,9 @@ const LogIn = () => {
                                     <input style={{border: mode ? value.email.trim().length !== 0 ? "2px solid green" : "2px solid red" : ""}} value={value.email} onChange={handleChange} name={"email"} type="email" placeholder="email" className=""/>
                                     <input style={{border: mode ? value.password.trim().length !== 0 ? "2px solid green" : "2px solid red" : ""}} value={value.password} onChange={handleChange} name={"password"} type="password" placeholder="password" className=""/>
                                     <button onClick={formChange} className="logIn--nav__button">Continue</button>
-                                    <button className="logIn--nav__child"><FcGoogle className="text-3xl mx-2"/>Continue with Google</button>
                                 </div>
                             </form>
+                            <button className="logIn--nav__child"><FcGoogle className="text-3xl mx-2"/>Continue with Google</button>
                         </div>
                     </div>
                 </div>
