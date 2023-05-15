b06020f6a2d778575000fee591690a83542d2501
adilbek210505 <116545639+adilbek210505@users.noreply.github.com>
2023-05-16 02:05:19 +0600

    init

--- src/store/Reducer/ActionCreators/BasketCreators.ts
+++ src/store/Reducer/ActionCreators/BasketCreators.ts
@@ -3,16 +3,43 @@ import {addToBasket, basketOnes, decBasket, deletesBasket, TBasket} from "../Bas
 
 
 export const addToBasketsREC = (el: TBasket) => (dispatch: appDispatch) => {
+    const baskets: any  = localStorage.getItem("basket")
+    let block = JSON.parse(baskets) || []
+    const fount = block.find((e: any) => e.id === el.id)
+    if (fount) {
+        block = block.map((e: any) => e.id === fount.id ? {...e, quantity: e.quantity +1} : e)
+    } else {
+        block = [...block, {...el, quantity: 1}]
+    }
     dispatch(addToBasket(el))
+    localStorage.setItem("basket", JSON.stringify(block))
 }
 export const decBasketsREC = (el: any) => (dispatch: appDispatch) => {
+    const baskets: any  = localStorage.getItem("basket")
+    let task = JSON.parse(baskets) || []
+    task = task.map((e: any) => {
+        if (e.id === el.id) {
+            if (e.quantity > 1) {
+                return  {...e, quantity: e.quantity -1}
+            } else return e
+        } else return e
+    })
     dispatch(decBasket(el))
+    localStorage.setItem("basket", JSON.stringify(task))
 }
 
 export const deleteBasketsREC = (el: any) => (dispatch: appDispatch) => {
+    const baskets: any  = localStorage.getItem("basket")
+    let task = JSON.parse(baskets) || []
+    task = task.filter((e: any) => e.id !== el.id)
     dispatch(deletesBasket(el))
+    localStorage.setItem("basket", JSON.stringify(task))
 }
 
 export const basketDetail = (el: any) => (dispatch: appDispatch) => {
+    const basketsOne: any  = localStorage.getItem("basketOne")
+    let man = JSON.parse(basketsOne) || {}
+    man = el
     dispatch(basketOnes(el))
+    localStorage.setItem("basketOne", JSON.stringify(man))
 }
--- src/store/Reducer/ActionCreators/FavoriteCreators.ts
+++ src/store/Reducer/ActionCreators/FavoriteCreators.ts
@@ -3,5 +3,14 @@ import {IFavorite} from "../../../types";
 import {getToFavorite} from "../FavoriteSlice";
 
 export const addToFavorite = (el: IFavorite) => (dispatch: appDispatch) => {
+    const fav: any = localStorage.getItem("favorite")
+    let con = JSON.parse(fav) || []
+    const fount = con.find((e: any) => e.id === el.id)
+    if (fount) {
+        con = con.filter((e: any) => el.id !== e.id)
+    } else {
+        con = [...con, {...el}]
+    }
     dispatch(getToFavorite(el))
+    localStorage.setItem("favorite", JSON.stringify(con))
 }
